26e03edaf2821d26023000ec8cfc34bbfb0592da
Simeon Vincent <svincent@gmail.com>
2025-11-04 17:41:08 -0800

    Add Safari support

--- js/background.js
+++ js/background.js
@@ -1,12 +1,16 @@
 const { contextMenus, runtime, tabs } = (globalThis.chrome || browser);
 
-contextMenus.create(
-  {
-    id: 'redirect',
-    title: 'Redirect',
-    contexts: ['all'],
-  },
-);
+runtime.onInstalled.addListener((details) => {
+  if (details.reason === 'install') {
+    contextMenus.create(
+      {
+        id: 'redirect',
+        title: 'Redirect',
+        contexts: ['all'],
+      },
+    );
+  }
+});
 
 contextMenus.onClicked.addListener((info, tab) => {
   tabs.sendMessage(tab.id, { url: tab.url }, () => runtime.lastError);
